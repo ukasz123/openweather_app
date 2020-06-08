a2eb813dfc80d1dae0ac4724862b35cd23695dd8
Łukasz Huculak <lukasz.huculak@codete.com>
2020-06-08 11:07:41 +0200

    Clean up

--- lib/services/openweather/client.dart
+++ lib/services/openweather/client.dart
@@ -49,7 +49,6 @@ class _OpenWeatherClient implements OpenWeatherClient {
       {bool forceRefresh: false}) async {
     Uri requestUri = Uri.https(
         _authority, '$_apiPath/weather', {'q': city, 'units': 'metric'});
-    print('Loading weather from remote');
     var jsonResponse = await _readJson(client.get(requestUri));
     return api_weather.Weather.fromCurrentWeatherResponse(city, jsonResponse);
   }
